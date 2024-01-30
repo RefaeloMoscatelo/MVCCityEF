4cb6b56d968ec7d40eb277b099adc0b598c2a92e
Rafael Moscatel <refaelomoscatelo@gmail.com>
2024-01-30 02:31:48 +0200

    Add project files.

---
+++ MVCCityEF/Controllers/CityController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCityEF.Controllers
+{
+    public class CityController : Controller
+    {
+        private DB_CityContext context = new DB_CityContext();
+        // GET: City
+        public ActionResult Index()
+        {
+            var cityList = context.tbl_cities.ToList();
+
+            return View(cityList);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tbl_cities city)
+        {
+            context.tbl_cities.Add(city);
+            context.SaveChanges();
+            ViewBag.Message = "City Added successfully";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            tbl_cities city = context.tbl_cities.Where(x => x.Id == id).FirstOrDefault();
+            return View(city);
+        }
+        [HttpPost]
+        public ActionResult Edit(tbl_cities Model)
+        {
+            var city = context.tbl_cities.Where(x => x.Id == Model.Id).FirstOrDefault();
+            if (city!=null)
+            {
+                city.Name = Model.Name;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var city = context.tbl_cities.Where(x => x.Id==id).FirstOrDefault();
+            context.tbl_cities.Remove(city);
+            context.SaveChanges();
+            ViewBag.Message = "City deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
